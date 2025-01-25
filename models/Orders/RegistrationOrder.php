249a45238ee4f7cd2d2b6f640a5364cfd175c423
Gabriel Solomon <hello@gabrielsolomon.ro>
2025-01-25 21:35:15 +0200

    feat: add currency from invoice to ordered items

--- models/Orders/RegistrationOrder.php
+++ models/Orders/RegistrationOrder.php
@@ -2,8 +2,40 @@
 
 namespace Sportic\OmniEvent\Models\Orders;
 
+use Spatie\SchemaOrg\Invoice;
 use Spatie\SchemaOrg\Order;
+use Spatie\SchemaOrg\OrderItem;
 
+/**
+ *
+ */
 class RegistrationOrder extends Order
 {
+
+    /**
+     * @return Invoice|null
+     */
+    public function getInvoice()
+    {
+        return $this->getProperty('partOfInvoice');
+    }
+
+    /**
+     * @param OrderItem $orderedItem
+     * @return RegistrationOrder
+     */
+    public function orderedItem($orderedItem)
+    {
+        $invoice = $this->getInvoice();
+        if ($invoice) {
+            $totalPaymentDue = $invoice->getProperty('totalPaymentDue');
+            $currency = $totalPaymentDue->getProperty('currency');
+            $orderedItem = is_array($orderedItem) ? $orderedItem : [$orderedItem];
+            foreach ($orderedItem as $item) {
+                $orderItem = $item->getProperty('orderedItem');
+                $orderItem->setProperty('priceCurrency', $currency);
+            }
+        }
+        return parent::orderedItem($orderedItem);
+    }
 }
